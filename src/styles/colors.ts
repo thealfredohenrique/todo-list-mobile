95a73e06877a137a24f245d9c0451ee1dc96c3ef
Alfredo Henrique <thealfredohenrique@gmail.com>
2024-02-22 21:27:25 -0300

    feat: create home page
    * Added a new home page to the application.
    * The home page is styled to match the rest of the application.

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+export const colors = <const>{
+  blueDark: "#1e6f9f",
+  blue: "#4ea8de",
+
+  purpleDark: "#5e60ce",
+  purple: "#8284fa",
+
+  gray100: "#f2f2f2",
+  gray200: "#d9d9d9",
+  gray300: "#808080",
+  gray400: "#333333",
+  gray500: "#262626",
+  gray600: "#1a1a1a",
+  gray700: "#0d0d0d",
+
+  danger: "#e25858",
+};
